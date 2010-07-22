687a8ce51ea477d105fa233296edb1d33b1d7691
Stephen Czetty <stephen.czetty@bancvue.com>
2010-07-22 16:41:08 -0500

    More Tests

---
+++ src/UMMO.TestingUtils.Specs/RandomDataGenerator/When_getting_random_decimal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Machine.Specifications;
+using Rhino.Mocks;
+using UMMO.TestingUtils.RandomData;
+
+namespace UMMO.TestingUtils.Specs.RandomDataGenerator
+{
+    [Subject(typeof(RandomDecimal))]
+    public class When_getting_random_decimal : RandomDataGeneratorTestBase
+    {
+        private const decimal ExpectedDecimal = 22.5m;
+        private Establish Context = () => Random.Stub( x => x.NextDecimal() ).Return( ExpectedDecimal );
+        private Because Of = () => _randomDecimal = RandomDataGeneratorUnderTest.Decimal;
+
+        private It Should_be_of_type_random_decimal
+            = () => _randomDecimal.ShouldBeOfType< RandomDecimal >();
+
+        private It Should_return_expected_decimal_when_calling_value
+            = () => ( (RandomDecimal)_randomDecimal ).Value.ShouldEqual( ExpectedDecimal );
+
+        private It Should_implicitly_cast_to_decimal
+            = () =>
+                  {
+                      decimal value = (RandomDecimal)_randomDecimal;
+                      value.ShouldEqual( ExpectedDecimal );
+                  };
+
+        private static Object _randomDecimal;
+    }
+}
---
+++ src/UMMO.TestingUtils.Specs/RandomDataGenerator/When_getting_random_double.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Machine.Specifications;
+using Rhino.Mocks;
+using UMMO.TestingUtils.RandomData;
+
+namespace UMMO.TestingUtils.Specs.RandomDataGenerator
+{
+    [Subject(typeof(RandomDouble))]
+    public class When_getting_random_double : RandomDataGeneratorTestBase
+    {
+        private const double ExpectedDouble = 22.5;
+
+        private Establish Context = () =>
+                                        {
+                                            Random.Stub( x => x.NextDouble() ).Return( ExpectedDouble );
+                                            Random.Stub( x => x.Next() ).Return( 1 );
+                                        };
+
+        private Because Of = () => _randomDouble = RandomDataGeneratorUnderTest.Double;
+
+        private It Should_be_of_type_random_double
+            = () => _randomDouble.ShouldBeOfType< RandomDouble >();
+
+        private It Should_return_expected_double_when_calling_value
+            = () => ( (RandomDouble)_randomDouble ).Value.ShouldEqual( ExpectedDouble );
+
+        private It Should_implicitly_cast_to_double
+            = () =>
+                  {
+                      double value = (RandomDouble)_randomDouble;
+                      value.ShouldEqual( ExpectedDouble );
+                  };
+
+        private static Object _randomDouble;
+    }
+}
