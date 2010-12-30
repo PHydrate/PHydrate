a385c63cd6052d5fe35bc8aa4cec6465ee1a0f40
Stephen Czetty <stephen.czetty@bancvue.com>
2010-12-30 14:34:27 -0600

    Added tests for DictionaryExtensions

---
+++ src/PHydrate.Specs/Util/DictionaryExtensions/DictionaryExtensionsSpecificationBase.cs
@@ -0,0 +1,45 @@
+#region Copyright
+
+// This file is part of PHydrate.
+// 
+// PHydrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// PHydrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with PHydrate.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Copyright 2010, Stephen Michael Czetty
+
+#endregion
+
+using System.Collections.Generic;
+using Machine.Specifications;
+using Machine.Specifications.Annotations;
+using UMMO.TestingUtils;
+
+namespace PHydrate.Specs.Util.DictionaryExtensions
+{
+    public class DictionaryExtensionsSpecificationBase
+    {
+        protected static Dictionary< string, string > TestDictionary;
+        protected static string RandomKey;
+        protected static bool Result;
+        protected static string ActualName;
+
+        [ UsedImplicitly ]
+        private Establish Context = () => {
+                                        RandomKey = A.Random.String.Resembling.A.LastName;
+                                        TestDictionary = new Dictionary< string, string >
+                                                         { { RandomKey, A.Random.String } };
+                                        RandomKey.ShouldNotEqual( RandomKey.ToLower() );
+                                            // TODO: Create an UMMO method that ensures this
+                                    };
+    }
+}
---
+++ src/PHydrate.Specs/Util/DictionaryExtensions/When_checking_for_key_without_case.cs
@@ -0,0 +1,38 @@
+﻿#region Copyright
+
+// This file is part of PHydrate.
+// 
+// PHydrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// PHydrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with PHydrate.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Copyright 2010, Stephen Michael Czetty
+
+#endregion
+
+using Machine.Specifications;
+using PHydrate.Util;
+
+namespace PHydrate.Specs.Util.DictionaryExtensions
+{
+    [ Subject( typeof(PHydrate.Util.DictionaryExtensions) ) ]
+    public class When_checking_for_key_without_case : DictionaryExtensionsSpecificationBase
+    {
+        private Because Of = () => Result = TestDictionary.ContainsKeyNoCase( RandomKey.ToLower(), out ActualName );
+
+        private It Should_return_correct_name_in_output_parameter
+            = () => ActualName.ShouldEqual( RandomKey );
+
+        private It Should_return_true
+            = () => Result.ShouldBeTrue();
+    }
+}
