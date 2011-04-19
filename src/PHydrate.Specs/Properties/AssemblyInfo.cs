14dfdad1006caf3e75656d67cc09b8ce416ceb62
Stephen Czetty <stephen.czetty@bancvue.com>
2011-04-19 10:25:00 -0500

    Moved assembly version info into auto-created Version.cs

--- src/PHydrate.Specs/Properties/AssemblyInfo.cs
+++ src/PHydrate.Specs/Properties/AssemblyInfo.cs
@@ -31,7 +31,6 @@ using System.Runtime.InteropServices;
 [ assembly: AssemblyConfiguration( "" ) ]
 [ assembly: AssemblyCompany( "" ) ]
 [ assembly: AssemblyProduct( "PHydrate.Specs" ) ]
-[ assembly: AssemblyCopyright( "Copyright © Stephen Czetty 2010" ) ]
 [ assembly: AssemblyTrademark( "" ) ]
 [ assembly: AssemblyCulture( "" ) ]
 
@@ -45,17 +44,3 @@ using System.Runtime.InteropServices;
 
 [ assembly: Guid( "4e3f1f82-03c2-47e4-a10b-0c920ae29087" ) ]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("0.3.0.*")]
-//[ assembly: AssemblyFileVersion( "0.1.0.0" ) ]
-[assembly: AssemblyFileVersionAttribute("0.3.0.35")]
