3aed1635ef9d781917aabfbcab52fe9a1db93278
Stephen Czetty <sczetty@wellmed.net>
2011-08-09 12:03:39 -0500

    Removing local copy of machine.specifications source

--- extern/machine.specifications/Source/SharedAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyCompany("Machine Project")]
-[assembly: AssemblyProduct("Machine.Specifications")]
-[assembly: AssemblyCopyright("Copyright © Machine Project 2008, 2009, 2010, 2011")]
-[assembly: AssemblyTrademark("")]
--- extern/machine.specifications/Source/VersionInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyInformationalVersion("0.4.0.0")]
